673c86adb92d9baa770ca73f37c80b704c793964
xwei16 <xwei16@fa25-cs511-079.cs.illinois.edu>
2025-11-03 21:44:38 -0600

    hw2

--- test_hnsw.py
+++ test_hnsw.py
@@ -6,7 +6,7 @@ def test_evaluate_hnsw():
     
     expected = [932085, 934876, 561813, 708177, 706771, 695756, 435345, 701258, 455537, 872728]
    
-    with open("part0/output.txt", "r") as output_file:
+    with open("output.txt", "r") as output_file:
         output_lines = output_file.readlines()
     
     output_lines = [int(index.strip()) for index in output_lines]
